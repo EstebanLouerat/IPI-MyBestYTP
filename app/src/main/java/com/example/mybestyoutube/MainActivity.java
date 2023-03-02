3dc828263fe7c174b66794afababb49bd306b9df
Steb <72015326+EstebanLouerat@users.noreply.github.com>
2023-03-02 17:05:06 +0100

    Add new splash theme and logo

--- app/src/main/java/com/example/mybestyoutube/MainActivity.java
+++ app/src/main/java/com/example/mybestyoutube/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mybestyoutube;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/mybestyoutube/SplashScreen.java
+++ app/src/main/java/com/example/mybestyoutube/SplashScreen.java
@@ -1,7 +1,9 @@
 package com.example.mybestyoutube;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +14,12 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        nextActivity();
+    }
+    private void nextActivity() {
+        startActivity(new Intent(SplashScreen.this, MainActivity.class));
+        finish();
     }
 }
