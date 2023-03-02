357f7359fcccce78a445974e19c1430ab687df52
Steb <72015326+EstebanLouerat@users.noreply.github.com>
2023-03-02 16:40:01 +0100

    Add new splash theme and logo

---
+++ app/src/main/java/com/example/mybestyoutube/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.mybestyoutube;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
